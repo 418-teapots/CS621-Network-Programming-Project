dcc07d2946cc80b4744602450326892f47d8d53b
Domingo <yhuang158@dons.usfca.edu>
2019-02-21 15:06:26 -0800

    Fixed unused variable 't' bug

--- ns-3.29/examples/routing/cs621-dev01.cc
+++ ns-3.29/examples/routing/cs621-dev01.cc
@@ -78,7 +78,7 @@ main (int argc, char *argv[])
   // Allow the user to override any of the defaults and the above
   // DefaultValue::Bind ()s at run-time, via command-line arguments
   CommandLine cmd;
-  int t = 100; //fixed threshold t = 100 ms
+ // int t = 100; //fixed threshold t = 100 ms
   bool enableFlowMonitor = false;
   long compressionLinkCapacity = 0;
   cmd.AddNonOption("CompressionLinkCapacity", "Specify the maximum bandwidth", compressionLinkCapacity);
