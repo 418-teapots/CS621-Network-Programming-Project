0bbc691a026ca16563f24a74964311c0334a722f
Tae Lee <leeth7830@gmail.com>
2019-03-01 17:13:12 -0800

    Update cs621-dev01.cc

--- ns-3.29/examples/routing/cs621-dev01.cc
+++ ns-3.29/examples/routing/cs621-dev01.cc
@@ -157,7 +157,7 @@ main (int argc, char *argv[])
   apps.Start (Seconds (2.0));
   apps.Stop (Seconds (20.0));
   srandom(getpt());
-  uint8_t random_data[] = new uint8_t[packetSize]
+  uint8_t random_data[packetSize];
   for (int i=0; i<packetSize-1; i++)
     random_data[i]=(char)(random()&0x000000ff);
   
