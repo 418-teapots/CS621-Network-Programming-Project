361b903367193c22e39124b1044eb369b944f0b9
Tae <leeth7830@gmail.com>
2019-03-04 11:29:42 -0800

    run over 1~10Mbps

--- run.py
+++ run.py
@@ -1,1 +1,6 @@
 ##run this file to simulate for different capacity
+import os
+capList = [1,2,3,4,5,6,7,8,9,10]
+for cap in capList:
+    print ('Running for capacity: ' + str(cap) + 'Mbps')
+    os.system('cd ns-3.29' + ' && ' + './waf --run "cs621-dev01 --cap=' + str(cap) + '"')
