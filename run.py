73a2a67cb0e22de067b3b7c1e00bb37bbaae7ed1
Kei Fukutani <kfukutani@usfca.edu>
2019-03-25 14:52:06 -0700

    added makeGraph function.

--- run.py
+++ run.py
@@ -10,3 +10,22 @@ for cap in capList:
         else:
             print ('Running for capacity: ' + str(cap) + 'Mbps' + ' with compression')
             os.system('cd ns-3.29' + ' && ' + './waf --run "cs621-dev01 --cap=' + str(cap) + ' --comp=1"')
+
+
+result = [100, 300, 200, 500, 400, 100, 300, 200, 500, 400]
+
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# Make a graph. 
+def makeGraph(capacity, time_diff): 
+    plt.scatter(capacity, time_diff)
+    #plt.plot(capacity, time_diff)
+    plt.title("Simulation Result")
+    plt.xlabel("capacity")
+    plt.ylabel("Dt_H - Dt_L")
+    plt.show()
+
+capacity = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+time_diff = np.array(result)
+makeGraph(capacity, time_diff)
